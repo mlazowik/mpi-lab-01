2872a47a49a2aca63b375ab672ccf723b2098da8
Michał Łazowik <mlazowik@gmail.com>
2018-04-16 11:09:41 +0200

    Add random seed & proc number detection

--- hello-world-seq.c
+++ hello-world-seq.c
@@ -7,14 +7,28 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <sys/time.h>
 
 int main(int argc, char * argv[])
 {
   MPI_Init(&argc, &argv);
+
+  struct timeval start;
+  gettimeofday(&start, NULL);
+  srand(start.tv_usec);
+
+  int numProcesses, myRank;
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
+  if (myRank == 0) {
+    printf("Process 0 reporting number of processes: %d\n", numProcesses);
+  }
+
   unsigned t = rand() % 5;
   int64_t i=13;
   sleep(t);
-  printf("Hello world from %d/%d (slept %u s)!\n", 0, 1, t);
+  printf("Hello world from %d/%d (slept %u s)!\n", myRank, 1, t);
   MPI_Finalize();
   return 0;
 }
